60225073f871065bee15cdd20e64684c8cfaadad
BJ Dierkes <derks@datafolklabs.com>
2013-11-12 13:28:58 -0600

    Resolving django 1.6 issues

--- utils/drest.mockapi/mockapi/urls.py
+++ utils/drest.mockapi/mockapi/urls.py
@@ -1,14 +1,14 @@
 
 from django.http import HttpResponse
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from mockapi.api import v0_api
 
 def render_null(request):
     return HttpResponse('')
-    
+
 urlpatterns = patterns('',
     url(r'^api/', include(v0_api.urls)),
     url(r'^favicon.ico/$', render_null),
-    url(r'^fake_long_request/$', 
+    url(r'^fake_long_request/$',
         'mockapi.projects.views.fake_long_request')
 )
